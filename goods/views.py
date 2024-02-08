291c9d1cec5f310f420d1a44e3d19be8774915c1
IvanGoroshenya <ivangoroshenya@yandex.by>
2024-02-08 22:17:23 +0300

    fixtures

---
+++ goods/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+
+def catalog(request):
+    context = {
+        "title": "Home - Каталог",
+        "goods": [
+            {
+                "image": "deps/images/goods/set of tea table and three chairs.jpg",
+                "name": "Чайный столик и три стула",
+                "description": "Комплект из трёх стульев и дизайнерский столик для гостинной комнаты.",
+                "price": 150.00,
+            },
+            {
+                "image": "deps/images/goods/set of tea table and two chairs.jpg",
+                "name": "Чайный столик и два стула",
+                "description": "Набор из стола и двух стульев в минималистическом стиле.",
+                "price": 93.00,
+            },
+            {
+                "image": "deps/images/goods/double bed.jpg",
+                "name": "Двухспальная кровать",
+                "description": "Кровать двухспальная с надголовником и вообще очень ортопедичная.",
+                "price": 670.00,
+            },
+            
+        ],
+    }
+    return render(request, "goods/catalog.html",context)
+
+
+def product(request):
+    return render(request, "goods/product.html")
--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 
 
 def index(request):
+    categories = Categories.objects.all()   #В административной панели хранятся все категории оттуда и берем их 
     context = {
         'title':'World_Art - Главная',
-        'content':'Магазин World Art'
+        'content':'Магазин World Art',
+        'categories':categories
     }
     
     return render(request,'main/index.html',context)
